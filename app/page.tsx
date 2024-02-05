174541dc265e48203cc92c4744367601ff2e7702
ajaymoturi12 <ajaymoturi0@gmail.com>
2024-02-04 19:47:07 -0500

    updated site content to be my own

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,307 @@
+import { Suspense } from 'react';
+import { unstable_noStore as noStore } from 'next/cache';
+import Link from 'next/link';
+import Image from 'next/image';
+import facebookSign from 'public/facebook_sign.jpg';
+import netflixFinale from 'public/netflix_finale.jpg';
+import ethindiaStage from 'public/ethindia_stage.jpg';
+import rivian from 'public/rivian_view.jpg';
+import polo from 'public/polo_photo.jpg';
+import ethindia from 'public/ethindia_line.jpg';
+import gtios from 'public/gtios_logo.png';
+import avatar from 'app/avatar.jpeg';
+import armLogo from 'public/arm_logo.svg'
+import ViewCounter from 'app/blog/view-counter';
+import { PreloadResources } from 'app/preload';
+import {
+  getLeeYouTubeSubs,
+  getVercelYouTubeSubs,
+  getViewsCount,
+} from 'app/db/queries';
+
+function Badge(props) {
+  return (
+    <a
+      {...props}
+      target="_blank"
+      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline"
+    />
+  );
+}
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+function ChannelLink({ img, link, name }) {
+  return (
+    <div className="group flex w-full">
+      <a
+        href={link}
+        target="_blank"
+        className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
+      >
+        <div className="flex items-center space-x-3">
+          <div className="relative h-16">
+            <Image
+              alt={name}
+              src={img}
+              height={64}
+              width={64}
+              sizes="33vw"
+              className="border border-neutral-200 dark:border-neutral-700 rounded-full h-16 w-16"
+              priority
+            />
+          </div>
+          <div className="flex flex-col">
+            <p className="font-medium text-neutral-900 dark:text-neutral-100">
+              {name}
+            </p>
+          </div>
+        </div>
+        <div className="text-neutral-700 dark:text-neutral-300 transform transition-transform duration-300 group-hover:-rotate-12">
+          <ArrowIcon />
+        </div>
+      </a>
+    </div>
+  );
+}
+
+function BlogLink({ slug, name }) {
+  return (
+    <div className="group">
+      <a
+        href={`/blog/${slug}`}
+        className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
+      >
+        <div className="flex flex-col">
+          <p className="font-medium text-neutral-900 dark:text-neutral-100">
+            {name}
+          </p>
+          <Suspense fallback={<p className="h-6" />}>
+            <Views slug={slug} />
+          </Suspense>
+        </div>
+        <div className="text-neutral-700 dark:text-neutral-300 transform transition-transform duration-300 group-hover:-rotate-12">
+          <ArrowIcon />
+        </div>
+      </a>
+    </div>
+  );
+}
+
+async function Views({ slug }: { slug: string }) {
+  let views = await getViewsCount();
+  return <ViewCounter allViews={views} slug={slug} />;
+}
+
 export default function Page() {
   return (
     <section>
-      <h1 className="font-medium text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <PreloadResources />
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">
+        hey, I'm ajay 👋
+      </h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        {`I'm a student, software engineer, traveler, and catalyst. I'm currently `}
+        <Link href="/work">working</Link>
+        {` on a couple side projects as I finish my degree at `}
+        <span className="not-prose">
+          <Badge href="https://www.gatech.edu">
+            <img
+              alt='Georgia Tech logomark'
+              src='/georgia_tech_logo_white.svg'>
+            </img>
+            <p className='opacity-0'>Georgia Tech</p>
+          </Badge>
+        </span>
+        {` , where I'm studying Computer Science, more specifically `}
+        <Badge href="https://www.cc.gatech.edu">
+          🧠 Artificial Intelligence
+        </Badge>
+        {` and `}
+        <Badge href="https://www.cc.gatech.edu">
+          🤖 Embedded Systems
+        </Badge>
+        .
       </p>
+      <div className="columns-2 sm:columns-3 gap-4 my-8">
+        <div className="relative h-40 mb-4">
+          <Image
+            alt="Me and some of my team memnbers at Netflix"
+            src={netflixFinale}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-80 mb-4 sm:mb-0">
+          <Image
+            alt="Me Playing Polo, one of my favorite hobbies"
+            src={polo}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-[-16px] sm:object-center"
+          />
+        </div>
+        <div className="relative h-40 sm:h-80 sm:mb-4">
+          <Image
+            alt="The main stage at ETHIndia, an Ethereum Hackathon I attended while traveling for work in India"
+            src={ethindiaStage}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-top sm:object-center"
+          />
+        </div>
+        <div className="relative h-40 mb-4 sm:mb-0">
+          <Image
+            alt="Me standing in front of Facebook's sign on 1 Hacker Way"
+            src={facebookSign}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-40 mb-4">
+          <Image
+            alt="Roadtripping in a Rivian R1S, one of my favorite pieces of technology"
+            src={rivian}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-80">
+          <Image
+            alt="The line to enter ETHIndia, an Ethereum Hackathon I attended while traveling for work in India"
+            src={ethindia}
+            fill
+            sizes="(min-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          For several years, I've been a Senior Tech Lead for <a href="https://www.gtios.club"> Georgia Tech's iOS Club </a>, where
+          we help students find their passion for mobile development by building cool apps 📱. I also spend some free time
+          writing software and working on cool side projects. Check out some of my work on <a href="https://www.github.com/ajaymoturi12">GitHub:</a>
+        </p>
+      </div>
+      <div className="my-8 flex flex-col sm:flex-row space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 w-full">
+        <ChannelLink
+          img={avatar}
+          name="@ajaymoturi12"
+          link="https://www.github.com/ajaymoturi12"
+        />
+        <ChannelLink
+          img={gtios}
+          name="@gtiosclub"
+          link="https://www.github.com/gtiosclub"
+        />
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          One of my goals as I enter my career is to write more. Whether it be documenting
+          my experience, giving advice that can help people in similar positions as me, or
+          simply talking about cool new technology. You can keep me accountable by reading
+          my blog ... we'll see how this one goes 🙃
+        </p>
+      </div>
+      <div className="my-8 flex flex-col space-y-4 w-full">
+        {/* <BlogLink
+          name="What Makes A Great Developer Experience?"
+          slug="developer-experience-examples"
+        />
+        <BlogLink name="What is Developer Relations?" slug="devrel-at-vercel" /> */}
+        <BlogLink name="Check out my (still very new) blog" slug="" />
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          I like to invest spare cash into growing tech companies that I think could become
+          behemoths one day. Some of the companies I'm currently invested in:
+        </p>
+      </div>
+      <div className="my-8 flex flex-row space-x-2 w-full h-14 overflow-hidden">
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
+          <a href="https://rivian.com">
+            <Image
+              src="/rivian_logo.png"
+              alt="Rivian Logo"
+              height={180}
+              width={900}
+            ></Image>
+          </a>
+        </div>
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
+          <a href="https://novavax.com">
+            <Image
+              src="/novavax_logo.png"
+              alt="Novavax Logo"
+              width={600}
+              height={150}
+            ></Image>
+          </a>
+        </div>
+        <div className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4">
+          <a href="https://www.makeswift.com/blog/makeswift-is-joining-bigcommerce">
+            <Image
+              priority
+              src={armLogo}
+              alt="ARM Logo"
+            ></Image>
+          </a>
+        </div>
+      </div>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          Thanks for checking out my website :) If you found something interesting or
+          just want to chat, feel free to reach out below:
+        </p>
+      </div>
+      <ul className="flex flex-col md:flex-row mt-8 space-x-0 md:space-x-4 space-y-2 md:space-y-0 font-sm text-neutral-600 dark:text-neutral-300">
+        <li>
+          <a
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://linkedin.com/in/ajaymoturi"
+          >
+            <ArrowIcon />
+            <p className="h-7 ml-2">connect with me</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center hover:text-neutral-800 dark:hover:text-neutral-100 transition-all"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="mailto:ajaymoturi0@gmail.com"
+          >
+            <ArrowIcon />
+            <p className="h-7 ml-2">let's chat</p>
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,10 +1,188 @@
-export default function Page() {
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import Image from 'next/image'
+
+export const metadata: Metadata = {
+  title: 'Work',
+  description: 'A summary of my work and contributions.',
+};
+
+export default function WorkPage() {
+  console.log()
   return (
     <section>
       <h1 className="font-medium text-2xl mb-8 tracking-tighter">my work</h1>
-      <p className="prose prose-neutral dark:prose-invert">
-        This is where your work experience goes.
-      </p>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          I'm still early in my career, but striving to build great products
+          that people love and bring something positive into the world. My goal
+          in life is to stay curious and learn a lot along the way. Here's a
+          summary of my work so far.
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Key</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Product, Engineering, & Growth Intern
+          <br></br>
+          August 2023 - Present
+        </p>
+
+        <p>
+          I joined <a href="https://key.ai">Key</a> as employee #1 to lead its engineering
+          organization and product development as we set out to build the de facto product
+          for career development, networking, and building impactful professional communities.
+        </p>
+        <h2 className="font-medium text-lg mb-1 tracking-tighter"> Some things I've at Key:</h2>
+        <ul>
+          <li>
+            Grew the company's product user base from 0 to 6.5k
+          </li>
+          <li>
+            Hired and managed a team of 13 full-time engineers & 2 designers
+            spanning 3 distinct product categories
+          </li>
+          <li>
+            Created and managed cloud infrastructure, CI/CD pipelines,
+            deployment logging, and engineering on-call systems
+          </li>
+          <li>
+            Conducted user interviews, orchestrated product feedback rounds,
+            and ran weekly sprints/release cycles
+          </li>
+          <li>
+            Hosted in-person and virtual events (like <a href="https://www.youtube.com/watch?v=udWephd7f8U">
+              this fireside chat</a> with <a href="https://linkedin.com/in/davidlieb">David Lieb</a>)
+            to bring together the 30k+ member Xoogler community.
+          </li>
+        </ul>
+        <p>
+          In case you were curious, I turned down a Google offer to work @ Key and learn about startups instead.
+          It was once of the best decisions of my life. I would highly encourage anyone who's remotely interested
+          in startups to take the jump and get out of your comfort zone. You won't regret it, and life is too short
+          to wonder "what if?".
+        </p>
+        <p>
+        </p>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Netflix</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Software Engineering Intern
+          <br></br>
+          Summer 2023
+        </p>
+        <p>
+          I had the pleasure of spending a summer working alongside the folks of
+          <a href="https://research.netflix.com/research-area/video-encoding-and-quality"> Netflix's
+            Video & Image Encoding and Video Codecs & Quality Teams</a>, helping them migrate from the company's
+          monolithic legacy streaming & encoding pipeline (dubbed 'Reloaded') to a new microservice
+          architecture built on Cosmos, a proprietary media-centric distributed computing platform.
+        </p>
+        <p>
+          Specifically, I built an end-to-end microservice to ensure that this new encoding pipeline
+          honored certain points of interest in the final bitstreams that reach devices in users'
+          homes and hands. Ever seen the "Skip Intro?" or "Skip Credits?" buttons that pop up on the
+          screen when you're binging your favorite shows (mine are Suits, Breaking Bad, & the Flash)?
+          Those are the kinds of POI's I was working with.
+        </p>
+        <p>
+          I give huge props 👏 to the entire Encoding Technologies org @ Netflix for accomplishing this
+          migration. It's no small feat, and the fact that not a single user would suspect this
+          happened says all you need to know. Check out the links below to learn more ⬇️
+        </p>
+
+        <div className="my-8 flex flex-col sm:flex-row space-x-0 sm:space-x-4 space-y-4 sm:space-y-0 w-full">
+          <div className="group flex w-full">
+            <a
+              href="https://netflixtechblog.com/the-netflix-cosmos-platform-35c14d9351ad"
+              target="_blank"
+              className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
+            >
+              <div className="flex items-center space-x-3">
+                <Image
+                  alt="Netflix Cosmos Microservice Architecture"
+                  src="/netflix_cosmos.png"
+                  height={150}
+                  width={150}
+                  priority
+                />
+                <div className="flex flex-col">
+                  <p className="font-medium text-neutral-900 dark:text-neutral-100">
+                  Cosmos Microservice Platform
+                  </p>
+                </div>
+              </div>
+              <div className="text-neutral-700 dark:text-neutral-300 transform transition-transform duration-300 group-hover:-rotate-12">
+                <ArrowIcon />
+              </div>
+            </a>
+          </div>
+          <div className="group flex w-full">
+            <a
+              href="https://netflixtechblog.com/rebuilding-netflix-video-processing-pipeline-with-microservices-4e5e6310e359"
+              target="_blank"
+              className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded flex items-center justify-between px-3 py-4 w-full"
+            >
+              <div className="flex items-center space-x-3">
+                <Image
+                  alt="Netflix Cosmos Microservice Architecture"
+                  src="/netflix_encoding_pipeline.png"
+                  height={150}
+                  width={150}
+                  priority
+                />
+                <div className="flex flex-col">
+                  <p className="font-medium text-neutral-900 dark:text-neutral-100">
+                    Video Encoding Pipeline Migration Process
+                  </p>
+                </div>
+              </div>
+              <div className="text-neutral-700 dark:text-neutral-300 transform transition-transform duration-300 group-hover:-rotate-12">
+                <ArrowIcon />
+              </div>
+            </a>
+          </div>
+        </div>
+        <hr className="my-6 border-neutral-100 dark:border-neutral-800" />
+        <h2 className="font-medium text-xl mb-1 tracking-tighter">Cisco</h2>
+        <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+          Software Engineering Intern
+          <br></br>
+          Summer 2022
+        </p>
+        <p>
+          Cisco powers much of the networking, cloud, and cybersecurity infrastructure and software that keeps us
+          connected to the internet 24/7. I got to work on the Polaris/IOS-XE team, which maintains the operating system
+          that runs on majority of Cisco's hardware product lines (routers, switches, links, etc.).
+        </p>
+        <p>
+          While I'll concede networking doesn't sound that cool, I got to work with some pretty cool tech 😎.
+          Specifically, I used Natural Language Processing & Machine Learning methods to build an internal developer tool
+          that made understanding device logs easier. Since these devices process millions of requests every second,
+          their logging files can get very dense, so I trained a classification model to parse these logs in their entirety
+          and summarize the observed behavior for other engineers to use during testing/debugging.
+        </p>
+        <p>
+          Fun fact, Cisco has been ranked the #️⃣1️⃣ place to work in the U.S. for many years running!
+          It was a pretty cool first job :)
+        </p>
+      </div>
     </section>
+  );
+}
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+        fill="currentColor"
+      />
+    </svg>
   );
 }
